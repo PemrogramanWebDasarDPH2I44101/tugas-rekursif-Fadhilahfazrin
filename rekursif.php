c6611e3678a38adaea03993c151b85968f632c08
Fadhilahfazrin <42725270+Fadhilahfazrin@users.noreply.github.com>
2018-09-10 19:00:49 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,22 @@
 <?php
+$b = 0;
+rekursif1(5);
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+function rekursif2($a, $kebalik){
+	global $b;
+	if ($a > $b - $kebalik) {
+		echo "*";
+		rekursif2($a - 1, $kebalik);
+	}
 }
-rekursif(5);
+
+function rekursif1($kebalik){
+	global $b;
+	rekursif2($b, $kebalik);
+	echo "<br>";
+	if (--$kebalik > $b) {
+		rekursif1($kebalik);
+	}
+}
+
 ?>
